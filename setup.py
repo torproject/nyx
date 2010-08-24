54bda8b3cab4df36553bc1a1a2485070f39764bd
Damian Johnson <atagar@torproject.org>
2010-08-24 16:49:42 +0000

    Finishing distutil changes to make arm installable. fix: adding '/usr/local/bin/arm' entry for the arm installation fix: directing build to /tmp to avoid cluttering the local arm directory fix: missing graphing moduile from install fix: deleting autogenerated egg metadata after setup
    svn:r23035

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
-
+import os
 from distutils.core import setup
 
+VERSION = '1.3.6_dev'
+
 setup(name='arm',
-      version='1.3.6_dev',
+      version=VERSION,
       description='Terminal tor status monitor',
       license='GPL v3',
       author='Damian Johnson',
       author_email='atagar@torproject.org',
       url='http://www.atagar.com/arm/',
-      packages=['arm', 'arm.interface', 'arm.util', 'arm.TorCtl'],
+      packages=['arm', 'arm.interface', 'arm.interface.graphing', 'arm.util', 'arm.TorCtl'],
       package_dir={'arm': 'src'},
+      scripts=["arm"],
      )
+
+# Removes the egg_info file. Apparently it is not optional during setup
+# (hardcoded in distutils/command/install.py), nor are there any arguments to
+# bypass its creation.
+eggPath = '/usr/local/arm-%s.egg-info' % VERSION
+if os.path.isfile(eggPath): os.remove(eggPath)
 
