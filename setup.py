587ff29db560e04fe557131116fcaee6dde93487
Damian Johnson <atagar@torproject.org>
2010-11-15 03:16:54 +0000

    Storing uncompressed man page and compressing them as part of the install process.
    svn:r23797

--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 import os
 import sys
+import gzip
 from src.version import VERSION
 from distutils.core import setup
+
+# Compresses the man page. This is a temporary file that we'll install. If
+# something goes wrong then we'll print the issue and use the uncompressed man
+# page instead. Build resources are cleaned up by the installion script later.
+try:
+  manInputFile = open('arm.1', 'r')
+  manContents = manInputFile.read()
+  manInputFile.close()
+  
+  if not os.path.exists('./build'): os.makedirs('./build')
+  manOutputFile = gzip.open('build/arm.1.gz', 'wb')
+  manOutputFile.write(manContents)
+  manOutputFile.close()
+  
+  manFilename = "build/arm.1.gz"
+except IOError, exc:
+  print "Unable to compress man page: %s" % exc
+  manFilename = "arm.1"
 
 setup(name='arm',
       version=VERSION,
@@ -14,8 +33,8 @@ setup(name='arm',
       packages=['arm', 'arm.interface', 'arm.interface.graphing', 'arm.util', 'arm.TorCtl'],
       package_dir={'arm': 'src'},
       data_files=[("/usr/bin", ["arm"]),
-                  ("/usr/lib/arm", ["src/settings.cfg"]),
-                  ("/usr/share/man/man1", ["debian/arm.1.gz"])],
+                  ("/usr/share/man/man1", [manFilename]),
+                  ("/usr/lib/arm", ["src/settings.cfg"])],
      )
 
 # Removes the egg_info file. Apparently it is not optional during setup
