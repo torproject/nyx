b53dd50dbf024eece4d56583646f526dfe40befe
Damian Johnson <atagar@torproject.org>
2010-11-18 16:21:55 +0000

    More changes for the deb: purging egg-info from the debian build revising path in the man page for docs to reflect debian's special location for them upping the Standards-Version
    svn:r23827

--- deb-prep.sh
+++ deb-prep.sh
@@ -8,6 +8,12 @@
 # tar czf tor-arm_1.3.7.orig.tar.gz --exclude-vcs --exclude="*.pyc" -v release
 
 svn export release release_build
+
+# edits the man page path for the sample armrc to reflect where it's located
+# on debian:
+# /usr/share/doc/arm/armrc.sample -> /usr/share/doc/tor-arm/armrc.sample.gz
+sed -i 's/\/usr\/share\/doc\/arm\/armrc.sample/\/usr\/share\/doc\/tor-arm\/armrc.sample.gz/g' release_build/arm.1
+
 tar czf tor-arm_1.3.7.orig.tar.gz release_build
 svn export resources/build/debian release_build/debian
 
