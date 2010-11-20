e57ec950074c0d5b9e6daaf059dd542b39648ac4
Damian Johnson <atagar@torproject.org>
2010-11-20 05:33:59 +0000

    More changes for deb builds: - Including flag so dh_pysupport will recognize the private python module (thanks, weasel and Emilio Pozuelo Monfort!) - Only performing man page compression when the --install flag is provided (preventing extra work and error messages)
    svn:r23838

--- setup.py
+++ setup.py
@@ -24,30 +24,30 @@ docPath = "/usr/share/doc/%s" % ("tor-arm" if isDebInstall else "arm")
 #   install-purelib=/usr/share
 # which would mean a bit more unnecessary clutter.
 
+manFilename = "arm.1"
 if "install" in sys.argv:
   sys.argv += ["--install-purelib", "/usr/share"]
-
-# Compresses the man page. This is a temporary file that we'll install. If
-# something goes wrong then we'll print the issue and use the uncompressed man
-# page instead.
-
-try:
-  manInputFile = open('arm.1', 'r')
-  manContents = manInputFile.read()
-  manInputFile.close()
   
-  # temporary destination for the man page guarenteed to be unoccupied (to
-  # avoid conflicting with files that are already there)
-  manOutputFile = gzip.open(tempfile.mktemp("/arm.1.gz"), 'wb')
-  manOutputFile.write(manContents)
-  manOutputFile.close()
+  # Compresses the man page. This is a temporary file that we'll install. If
+  # something goes wrong then we'll print the issue and use the uncompressed man
+  # page instead.
   
-  # places in tmp rather than a relative path to avoid having this copy appear
-  # in the deb and rpm builds
-  manFilename = manOutputFile.name
-except IOError, exc:
-  print "Unable to compress man page: %s" % exc
-  manFilename = "arm.1"
+  try:
+    manInputFile = open('arm.1', 'r')
+    manContents = manInputFile.read()
+    manInputFile.close()
+    
+    # temporary destination for the man page guarenteed to be unoccupied (to
+    # avoid conflicting with files that are already there)
+    manOutputFile = gzip.open(tempfile.mktemp("/arm.1.gz"), 'wb')
+    manOutputFile.write(manContents)
+    manOutputFile.close()
+    
+    # places in tmp rather than a relative path to avoid having this copy appear
+    # in the deb and rpm builds
+    manFilename = manOutputFile.name
+  except IOError, exc:
+    print "Unable to compress man page: %s" % exc
 
 setup(name='arm',
       version=VERSION,
