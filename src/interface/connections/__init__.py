01f9a43fff32b2fbf09f4a9b8e84fd67c2dad374
Damian Johnson <atagar@torproject.org>
2011-04-03 18:32:14 +0000

    Updating content listings with new modules.
    svn:r24551

--- src/interface/connections/__init__.py
+++ src/interface/connections/__init__.py
@@ -2,5 +2,5 @@
 Panels, popups, and handlers comprising the arm user interface.
 """
 
-__all__ = ["connEntry", "connPanel", "entries"]
+__all__ = ["circEntry", "connEntry", "connPanel", "entries"]
 
