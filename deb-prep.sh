1a3cbf38fd9cbffa42d211055201892b112e210c
Damian Johnson <atagar@torproject.org>
2011-04-21 07:57:06 -0700

    Converting deb prep to use git rather than svn

--- deb-prep.sh
+++ deb-prep.sh
@@ -12,13 +12,17 @@
 # alternate (works, but svn export is simpler):
 # tar czf tor-arm_1.3.7.orig.tar.gz --exclude-vcs --exclude="*.pyc" -v release
 
-svn export release release_deb
+export DEB_VERSION="1.2.3.4"
+
+mkdir release_deb
+git archive --format=tar release | (cd ./release_deb && tar xf -)
 
 # edits the man page path for the sample armrc to reflect where it's located
 # on debian:
 # /usr/share/doc/arm/armrc.sample -> /usr/share/doc/tor-arm/armrc.sample.gz
 sed -i 's/\/usr\/share\/doc\/arm\/armrc.sample/\/usr\/share\/doc\/tor-arm\/armrc.sample.gz/g' release_deb/arm.1
 
-tar czf tor-arm_1.4.2.0.orig.tar.gz release_deb
-svn export resources/build/debian release_deb/debian
+tar czf tor-arm_${DEB_VERSION}.orig.tar.gz release_deb
+
+(cd build && git archive --format=tar packaging debian) | (cd ./release_deb && tar xf -)
 
