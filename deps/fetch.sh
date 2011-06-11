5754dceffd68975c48966f17296df640cb2acd84
Damian Johnson <atagar@torproject.org>
2011-06-10 18:30:39 -0700

    fix: only using a subset of the cagraph archive
    We use the cagraph subdirectory of the tarball rather than the whole thing.

--- deps/fetch.sh
+++ deps/fetch.sh
@@ -6,7 +6,7 @@
 #   6/10/11 - be583e53b2bccf09a7126c5271f9af5682447903b6ac92cf1cf78ca5b35273ed
 # 
 # cagraph (https://code.google.com/p/cagraph/)
-#   6/10/11 - a6928f07adb8f8d4b0076e01c0ec264e1acaaa6db21376c854fa827c9b04e3f3
+#   6/10/11 - 1439acd40ce016f4329deb216d86f36a749e4b8bf73a313a757396af6f95310d
 
 # removes old archives if they exist
 [ -f "torctl.tar.gz" ] && rm -f "torctl.tar.gz"
@@ -22,8 +22,12 @@ rm -rf pytorctl
 
 # retrieves cagraph
 wget --quiet http://cagraph.googlecode.com/files/cagraph-1.2.tar.gz
+tar --strip-components=1 -xzf cagraph-1.2.tar.gz cagraph-1.2/cagraph
+tar -czf cagraph.tar.gz cagraph
+rm -rf cagraph/
+rm cagraph-1.2.tar.gz
 
 echo "Sha256 Checksums:"
 sha256sum torctl.tar.gz
-sha256sum cagraph-1.2.tar.gz
+sha256sum cagraph.tar.gz
 
