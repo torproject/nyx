d3ebb46af66d04be2f78905e8ea6d18c07ce6b52
Kamran Riaz Khan <krkhan@inspirated.com>
2011-07-11 01:04:11 +0500

    Display circuit lines appropriately.

--- src/gui/connections/circEntry.py
+++ src/gui/connections/circEntry.py
@@ -4,37 +4,39 @@ Connection panel entries for client circuits.
 
 import time
 
-from cli.connections import circEntry
-from util import gtkTools
+from cli.connections import circEntry, entries
+from gui.connections import connEntry
+from util import gtkTools, uiTools
 
 class CircEntry(circEntry.CircEntry):
   @classmethod
   def convertToGui(self, instance):
     instance.__class__ = self
 
-class CircHeaderLine(circEntry.CircHeaderLine):
+class CircHeaderLine(circEntry.CircHeaderLine, connEntry.ConnectionLine):
   @classmethod
   def convertToGui(self, instance):
     instance.__class__ = self
 
   def getListingRow(self, listingType):
-    local = "%s:%s" % (self.local.ipAddr, self.local.port)
-    foreign = "%s:%s" % (self.foreign.ipAddr, self.foreign.port)
-    timeLabel = "%d s" % (time.time() - self.startTime)
+    row = connEntry.ConnectionLine.getListingRow(self, listingType)
     theme = gtkTools.Theme()
+    return row[:-1] + (theme.colors['active'],)
 
-    return (local, foreign, timeLabel, self.baseType, theme.colors['active'])
-
-class CircLine(circEntry.CircLine):
+class CircLine(circEntry.CircLine, connEntry.ConnectionLine):
   @classmethod
   def convertToGui(self, instance):
     instance.__class__ = self
 
   def getListingRow(self, listingType):
-    local = "%s:%s" % (self.local.ipAddr, self.local.port)
-    foreign = "%s:%s" % (self.foreign.ipAddr, self.foreign.port)
-    timeLabel = "%d s" % (time.time() - self.startTime)
+    dst, etc = "", ""
+
+    if listingType == entries.ListingType.IP_ADDRESS:
+      dst = self.getDestinationLabel(100, includeLocale=True)
+      etc = self.foreign.getNickname()
+
+    timeLabel = uiTools.getTimeLabel(time.time() - self.startTime)
     theme = gtkTools.Theme()
 
-    return (local, foreign, timeLabel, self.baseType, theme.colors['normal'])
+    return (dst, etc, timeLabel, self.getType(), theme.colors['insensitive'])
 
