f700a3a4459fc2d0b5b1937199cbc94c1ba6d2f4
Damian Johnson <atagar@torproject.org>
2014-06-28 12:09:38 -0700

    Tests didn't reflect tracker's renamed methods
    We renamed a few methods in our tracker util but make them match in the tests,
    breaking our unit tests.

--- test/util/tracker/connection_tracker.py
+++ test/util/tracker/connection_tracker.py
@@ -17,21 +17,21 @@ class TestConnectionTracker(unittest.TestCase):
   @patch('arm.util.tracker.connection.get_connections')
   @patch('arm.util.tracker.system', Mock(return_value = Mock()))
   @patch('arm.util.tracker.connection.get_system_resolvers', Mock(return_value = [connection.Resolver.NETSTAT]))
-  def test_fetching_connections(self, get_connections_mock, tor_controller_mock):
+  def test_fetching_connections(self, get_value_mock, tor_controller_mock):
     tor_controller_mock().get_pid.return_value = 12345
-    get_connections_mock.return_value = [CONNECTION_1, CONNECTION_2, CONNECTION_3]
+    get_value_mock.return_value = [CONNECTION_1, CONNECTION_2, CONNECTION_3]
 
     with ConnectionTracker(0.04) as daemon:
       time.sleep(0.01)
 
-      connections = daemon.get_connections()
+      connections = daemon.get_value()
 
       self.assertEqual(1, daemon.run_counter())
       self.assertEqual([CONNECTION_1, CONNECTION_2, CONNECTION_3], connections)
 
-      get_connections_mock.return_value = []  # no connection results
+      get_value_mock.return_value = []  # no connection results
       time.sleep(0.05)
-      connections = daemon.get_connections()
+      connections = daemon.get_value()
 
       self.assertEqual(2, daemon.run_counter())
       self.assertEqual([], connections)
@@ -40,37 +40,37 @@ class TestConnectionTracker(unittest.TestCase):
   @patch('arm.util.tracker.connection.get_connections')
   @patch('arm.util.tracker.system', Mock(return_value = Mock()))
   @patch('arm.util.tracker.connection.get_system_resolvers', Mock(return_value = [connection.Resolver.NETSTAT, connection.Resolver.LSOF]))
-  def test_resolver_failover(self, get_connections_mock, tor_controller_mock):
+  def test_resolver_failover(self, get_value_mock, tor_controller_mock):
     tor_controller_mock().get_pid.return_value = 12345
-    get_connections_mock.side_effect = IOError()
+    get_value_mock.side_effect = IOError()
 
     with ConnectionTracker(0.01) as daemon:
       time.sleep(0.03)
 
       self.assertEqual([connection.Resolver.NETSTAT, connection.Resolver.LSOF], daemon._resolvers)
-      self.assertEqual([], daemon.get_connections())
+      self.assertEqual([], daemon.get_value())
 
       time.sleep(0.05)
 
       self.assertEqual([connection.Resolver.LSOF], daemon._resolvers)
-      self.assertEqual([], daemon.get_connections())
+      self.assertEqual([], daemon.get_value())
 
       time.sleep(0.05)
 
       self.assertEqual([], daemon._resolvers)
-      self.assertEqual([], daemon.get_connections())
+      self.assertEqual([], daemon.get_value())
 
       # Now make connection resolution work. We still shouldn't provide any
       # results since we stopped looking.
 
-      get_connections_mock.return_value = [CONNECTION_1, CONNECTION_2]
-      get_connections_mock.side_effect = None
+      get_value_mock.return_value = [CONNECTION_1, CONNECTION_2]
+      get_value_mock.side_effect = None
       time.sleep(0.05)
-      self.assertEqual([], daemon.get_connections())
+      self.assertEqual([], daemon.get_value())
 
       # Finally, select a custom resolver. This should cause us to query again
       # reguardless of our prior failures.
 
       daemon.set_custom_resolver(connection.Resolver.NETSTAT)
       time.sleep(0.05)
-      self.assertEqual([CONNECTION_1, CONNECTION_2], daemon.get_connections())
+      self.assertEqual([CONNECTION_1, CONNECTION_2], daemon.get_value())
