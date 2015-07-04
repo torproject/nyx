d06cec4899bf0987c412afd7e8f55ad8045939d8
Damian Johnson <atagar@torproject.org>
2015-07-04 13:10:52 -0700

    Consistently use 'with' for locking
    As we've been rewriting components I've swapping locking to use the 'with'
    keyword (much safer, avoiding deadlocks in case of exceptions). Swapping the
    rest of the codebase over so we're consistent.

--- nyx/connections/conn_panel.py
+++ nyx/connections/conn_panel.py
@@ -148,10 +148,7 @@ class ConnectionPanel(panel.Panel, threading.Thread):
     time we were last paused.
     """
 
-    if self._halt_time:
-      return self._halt_time
-    else:
-      return panel.Panel.get_pause_time(self)
+    return self._halt_time if self._halt_time else panel.Panel.get_pause_time(self)
 
   def set_sort_order(self, ordering = None):
     """
@@ -162,22 +159,19 @@ class ConnectionPanel(panel.Panel, threading.Thread):
                  set ordering
     """
 
-    self.vals_lock.acquire()
+    with self.vals_lock:
+      if ordering:
+        nyx_config = conf.get_config('nyx')
 
-    if ordering:
-      nyx_config = conf.get_config('nyx')
+        ordering_keys = [entries.SortAttr.keys()[entries.SortAttr.index_of(v)] for v in ordering]
+        nyx_config.set('features.connection.order', ', '.join(ordering_keys))
 
-      ordering_keys = [entries.SortAttr.keys()[entries.SortAttr.index_of(v)] for v in ordering]
-      nyx_config.set('features.connection.order', ', '.join(ordering_keys))
+      self._entries.sort(key = lambda i: (i.get_sort_values(CONFIG['features.connection.order'], self.get_listing_type())))
 
-    self._entries.sort(key = lambda i: (i.get_sort_values(CONFIG['features.connection.order'], self.get_listing_type())))
+      self._entry_lines = []
 
-    self._entry_lines = []
-
-    for entry in self._entries:
-      self._entry_lines += entry.getLines()
-
-    self.vals_lock.release()
+      for entry in self._entries:
+        self._entry_lines += entry.getLines()
 
   def get_listing_type(self):
     """
@@ -197,17 +191,14 @@ class ConnectionPanel(panel.Panel, threading.Thread):
     if self.get_listing_type() == listing_type:
       return
 
-    self.vals_lock.acquire()
+    with self.vals_lock:
+      nyx_config = conf.get_config('nyx')
+      nyx_config.set('features.connection.listing_type', Listing.keys()[Listing.index_of(listing_type)])
 
-    nyx_config = conf.get_config('nyx')
-    nyx_config.set('features.connection.listing_type', Listing.keys()[Listing.index_of(listing_type)])
+      # if we're sorting by the listing then we need to resort
 
-    # if we're sorting by the listing then we need to resort
-
-    if entries.SortAttr.LISTING in CONFIG['features.connection.order']:
-      self.set_sort_order()
-
-    self.vals_lock.release()
+      if entries.SortAttr.LISTING in CONFIG['features.connection.order']:
+        self.set_sort_order()
 
   def is_clients_allowed(self):
     """
@@ -423,86 +414,83 @@ class ConnectionPanel(panel.Panel, threading.Thread):
     return self._scroller.get_cursor_selection(self._entry_lines)
 
   def draw(self, width, height):
-    self.vals_lock.acquire()
+    with self.vals_lock:
+      # if we don't have any contents then refuse to show details
 
-    # if we don't have any contents then refuse to show details
+      if not self._entries:
+        self._show_details = False
 
-    if not self._entries:
-      self._show_details = False
+      # extra line when showing the detail panel is for the bottom border
 
-    # extra line when showing the detail panel is for the bottom border
+      detail_panel_offset = DETAILS_HEIGHT + 1 if self._show_details else 0
+      is_scrollbar_visible = len(self._entry_lines) > height - detail_panel_offset - 1
 
-    detail_panel_offset = DETAILS_HEIGHT + 1 if self._show_details else 0
-    is_scrollbar_visible = len(self._entry_lines) > height - detail_panel_offset - 1
+      scroll_location = self._scroller.get_scroll_location(self._entry_lines, height - detail_panel_offset - 1)
+      cursor_selection = self.get_selection()
 
-    scroll_location = self._scroller.get_scroll_location(self._entry_lines, height - detail_panel_offset - 1)
-    cursor_selection = self.get_selection()
+      # draws the detail panel if currently displaying it
 
-    # draws the detail panel if currently displaying it
+      if self._show_details and cursor_selection:
+        # This is a solid border unless the scrollbar is visible, in which case a
+        # 'T' pipe connects the border to the bar.
 
-    if self._show_details and cursor_selection:
-      # This is a solid border unless the scrollbar is visible, in which case a
-      # 'T' pipe connects the border to the bar.
+        ui_tools.draw_box(self, 0, 0, width, DETAILS_HEIGHT + 2)
 
-      ui_tools.draw_box(self, 0, 0, width, DETAILS_HEIGHT + 2)
+        if is_scrollbar_visible:
+          self.addch(DETAILS_HEIGHT + 1, 1, curses.ACS_TTEE)
+
+        draw_entries = cursor_selection.get_details(width)
+
+        for i in range(min(len(draw_entries), DETAILS_HEIGHT)):
+          self.addstr(1 + i, 2, draw_entries[i][0], *draw_entries[i][1])
+
+      # title label with connection counts
+
+      if self.is_title_visible():
+        title = 'Connection Details:' if self._show_details else self._title
+        self.addstr(0, 0, title, curses.A_STANDOUT)
+
+      scroll_offset = 0
 
       if is_scrollbar_visible:
-        self.addch(DETAILS_HEIGHT + 1, 1, curses.ACS_TTEE)
+        scroll_offset = 2
+        self.add_scroll_bar(scroll_location, scroll_location + height - detail_panel_offset - 1, len(self._entry_lines), 1 + detail_panel_offset)
 
-      draw_entries = cursor_selection.get_details(width)
+      if self.is_paused() or not self._is_tor_running:
+        current_time = self.get_pause_time()
+      else:
+        current_time = time.time()
 
-      for i in range(min(len(draw_entries), DETAILS_HEIGHT)):
-        self.addstr(1 + i, 2, draw_entries[i][0], *draw_entries[i][1])
+      for line_number in range(scroll_location, len(self._entry_lines)):
+        entry_line = self._entry_lines[line_number]
 
-    # title label with connection counts
+        # if this is an unresolved SOCKS, HIDDEN, or CONTROL entry then queue up
+        # resolution for the applicaitions they belong to
 
-    if self.is_title_visible():
-      title = 'Connection Details:' if self._show_details else self._title
-      self.addstr(0, 0, title, curses.A_STANDOUT)
+        if isinstance(entry_line, conn_entry.ConnectionLine) and entry_line.is_unresolved_application():
+          self._resolve_apps()
 
-    scroll_offset = 0
+        # hilighting if this is the selected line
 
-    if is_scrollbar_visible:
-      scroll_offset = 2
-      self.add_scroll_bar(scroll_location, scroll_location + height - detail_panel_offset - 1, len(self._entry_lines), 1 + detail_panel_offset)
+        extra_format = curses.A_STANDOUT if entry_line == cursor_selection else curses.A_NORMAL
 
-    if self.is_paused() or not self._is_tor_running:
-      current_time = self.get_pause_time()
-    else:
-      current_time = time.time()
+        draw_line = line_number + detail_panel_offset + 1 - scroll_location
 
-    for line_number in range(scroll_location, len(self._entry_lines)):
-      entry_line = self._entry_lines[line_number]
+        prefix = entry_line.get_listing_prefix()
 
-      # if this is an unresolved SOCKS, HIDDEN, or CONTROL entry then queue up
-      # resolution for the applicaitions they belong to
+        for i in range(len(prefix)):
+          self.addch(draw_line, scroll_offset + i, prefix[i])
 
-      if isinstance(entry_line, conn_entry.ConnectionLine) and entry_line.is_unresolved_application():
-        self._resolve_apps()
+        x_offset = scroll_offset + len(prefix)
+        draw_entry = entry_line.get_listing_entry(width - scroll_offset - len(prefix), current_time, self.get_listing_type())
 
-      # hilighting if this is the selected line
+        for msg, attr in draw_entry:
+          attr |= extra_format
+          self.addstr(draw_line, x_offset, msg, attr)
+          x_offset += len(msg)
 
-      extra_format = curses.A_STANDOUT if entry_line == cursor_selection else curses.A_NORMAL
-
-      draw_line = line_number + detail_panel_offset + 1 - scroll_location
-
-      prefix = entry_line.get_listing_prefix()
-
-      for i in range(len(prefix)):
-        self.addch(draw_line, scroll_offset + i, prefix[i])
-
-      x_offset = scroll_offset + len(prefix)
-      draw_entry = entry_line.get_listing_entry(width - scroll_offset - len(prefix), current_time, self.get_listing_type())
-
-      for msg, attr in draw_entry:
-        attr |= extra_format
-        self.addstr(draw_line, x_offset, msg, attr)
-        x_offset += len(msg)
-
-      if draw_line >= height:
-        break
-
-    self.vals_lock.release()
+        if draw_line >= height:
+          break
 
   def stop(self):
     """
@@ -528,114 +516,112 @@ class ConnectionPanel(panel.Panel, threading.Thread):
     conn_resolver = nyx.util.tracker.get_connection_tracker()
     current_resolution_count = conn_resolver.run_counter()
 
-    self.vals_lock.acquire()
+    with self.vals_lock:
+      new_entries = []  # the new results we'll display
 
-    new_entries = []  # the new results we'll display
+      # Fetches new connections and client circuits...
+      # new_connections  [(local ip, local port, foreign ip, foreign port)...]
+      # new_circuits     {circuit_id => (status, purpose, path)...}
 
-    # Fetches new connections and client circuits...
-    # new_connections  [(local ip, local port, foreign ip, foreign port)...]
-    # new_circuits     {circuit_id => (status, purpose, path)...}
+      new_connections = [(conn.local_address, conn.local_port, conn.remote_address, conn.remote_port) for conn in conn_resolver.get_value()]
+      new_circuits = {}
 
-    new_connections = [(conn.local_address, conn.local_port, conn.remote_address, conn.remote_port) for conn in conn_resolver.get_value()]
-    new_circuits = {}
+      for circ in tor_controller().get_circuits([]):
+        # Skips established single-hop circuits (these are for directory
+        # fetches, not client circuits)
 
-    for circ in tor_controller().get_circuits([]):
-      # Skips established single-hop circuits (these are for directory
-      # fetches, not client circuits)
+        if not (circ.status == 'BUILT' and len(circ.path) == 1):
+          new_circuits[circ.id] = (circ.status, circ.purpose, [entry[0] for entry in circ.path])
 
-      if not (circ.status == 'BUILT' and len(circ.path) == 1):
-        new_circuits[circ.id] = (circ.status, circ.purpose, [entry[0] for entry in circ.path])
+      # Populates new_entries with any of our old entries that still exist.
+      # This is both for performance and to keep from resetting the uptime
+      # attributes. Note that CircEntries are a ConnectionEntry subclass so
+      # we need to check for them first.
 
-    # Populates new_entries with any of our old entries that still exist.
-    # This is both for performance and to keep from resetting the uptime
-    # attributes. Note that CircEntries are a ConnectionEntry subclass so
-    # we need to check for them first.
+      for old_entry in self._entries:
+        if isinstance(old_entry, circ_entry.CircEntry):
+          new_entry = new_circuits.get(old_entry.circuit_id)
 
-    for old_entry in self._entries:
-      if isinstance(old_entry, circ_entry.CircEntry):
-        new_entry = new_circuits.get(old_entry.circuit_id)
+          if new_entry:
+            old_entry.update(new_entry[0], new_entry[2])
+            new_entries.append(old_entry)
+            del new_circuits[old_entry.circuit_id]
+        elif isinstance(old_entry, conn_entry.ConnectionEntry):
+          connection_line = old_entry.getLines()[0]
+          conn_attr = (connection_line.local.get_address(), connection_line.local.get_port(),
+                       connection_line.foreign.get_address(), connection_line.foreign.get_port())
 
-        if new_entry:
-          old_entry.update(new_entry[0], new_entry[2])
-          new_entries.append(old_entry)
-          del new_circuits[old_entry.circuit_id]
-      elif isinstance(old_entry, conn_entry.ConnectionEntry):
-        connection_line = old_entry.getLines()[0]
-        conn_attr = (connection_line.local.get_address(), connection_line.local.get_port(),
-                     connection_line.foreign.get_address(), connection_line.foreign.get_port())
+          if conn_attr in new_connections:
+            new_entries.append(old_entry)
+            new_connections.remove(conn_attr)
 
-        if conn_attr in new_connections:
-          new_entries.append(old_entry)
-          new_connections.remove(conn_attr)
+      # Reset any display attributes for the entries we're keeping
 
-    # Reset any display attributes for the entries we're keeping
+      for entry in new_entries:
+        entry.reset_display()
 
-    for entry in new_entries:
-      entry.reset_display()
+      # Adds any new connection and circuit entries.
 
-    # Adds any new connection and circuit entries.
+      for local_address, local_port, remote_address, remote_port in new_connections:
+        new_conn_entry = conn_entry.ConnectionEntry(local_address, local_port, remote_address, remote_port)
+        new_conn_line = new_conn_entry.getLines()[0]
 
-    for local_address, local_port, remote_address, remote_port in new_connections:
-      new_conn_entry = conn_entry.ConnectionEntry(local_address, local_port, remote_address, remote_port)
-      new_conn_line = new_conn_entry.getLines()[0]
+        if new_conn_line.get_type() != conn_entry.Category.CIRCUIT:
+          new_entries.append(new_conn_entry)
 
-      if new_conn_line.get_type() != conn_entry.Category.CIRCUIT:
-        new_entries.append(new_conn_entry)
+          # updates exit port and client locale usage information
+          if new_conn_line.is_private():
+            if new_conn_line.get_type() == conn_entry.Category.INBOUND:
+              # client connection, update locale information
 
-        # updates exit port and client locale usage information
-        if new_conn_line.is_private():
-          if new_conn_line.get_type() == conn_entry.Category.INBOUND:
-            # client connection, update locale information
+              client_locale = new_conn_line.foreign.get_locale()
 
-            client_locale = new_conn_line.foreign.get_locale()
+              if client_locale:
+                self._client_locale_usage[client_locale] = self._client_locale_usage.get(client_locale, 0) + 1
+            elif new_conn_line.get_type() == conn_entry.Category.EXIT:
+              exit_port = new_conn_line.foreign.get_port()
+              self._exit_port_usage[exit_port] = self._exit_port_usage.get(exit_port, 0) + 1
 
-            if client_locale:
-              self._client_locale_usage[client_locale] = self._client_locale_usage.get(client_locale, 0) + 1
-          elif new_conn_line.get_type() == conn_entry.Category.EXIT:
-            exit_port = new_conn_line.foreign.get_port()
-            self._exit_port_usage[exit_port] = self._exit_port_usage.get(exit_port, 0) + 1
+      for circuit_id in new_circuits:
+        status, purpose, path = new_circuits[circuit_id]
+        new_entries.append(circ_entry.CircEntry(circuit_id, status, purpose, path))
 
-    for circuit_id in new_circuits:
-      status, purpose, path = new_circuits[circuit_id]
-      new_entries.append(circ_entry.CircEntry(circuit_id, status, purpose, path))
+      # Counts the relays in each of the categories. This also flushes the
+      # type cache for all of the connections (in case its changed since last
+      # fetched).
 
-    # Counts the relays in each of the categories. This also flushes the
-    # type cache for all of the connections (in case its changed since last
-    # fetched).
+      category_types = list(conn_entry.Category)
+      type_counts = dict((type, 0) for type in category_types)
 
-    category_types = list(conn_entry.Category)
-    type_counts = dict((type, 0) for type in category_types)
+      for entry in new_entries:
+        if isinstance(entry, conn_entry.ConnectionEntry):
+          type_counts[entry.getLines()[0].get_type()] += 1
+        elif isinstance(entry, circ_entry.CircEntry):
+          type_counts[conn_entry.Category.CIRCUIT] += 1
 
-    for entry in new_entries:
-      if isinstance(entry, conn_entry.ConnectionEntry):
-        type_counts[entry.getLines()[0].get_type()] += 1
-      elif isinstance(entry, circ_entry.CircEntry):
-        type_counts[conn_entry.Category.CIRCUIT] += 1
+      # makes labels for all the categories with connections (ie,
+      # "21 outbound", "1 control", etc)
 
-    # makes labels for all the categories with connections (ie,
-    # "21 outbound", "1 control", etc)
+      count_labels = []
 
-    count_labels = []
+      for category in category_types:
+        if type_counts[category] > 0:
+          count_labels.append('%i %s' % (type_counts[category], category.lower()))
 
-    for category in category_types:
-      if type_counts[category] > 0:
-        count_labels.append('%i %s' % (type_counts[category], category.lower()))
+      if count_labels:
+        self._title = 'Connections (%s):' % ', '.join(count_labels)
+      else:
+        self._title = 'Connections:'
 
-    if count_labels:
-      self._title = 'Connections (%s):' % ', '.join(count_labels)
-    else:
-      self._title = 'Connections:'
+      self._entries = new_entries
 
-    self._entries = new_entries
+      self._entry_lines = []
 
-    self._entry_lines = []
+      for entry in self._entries:
+        self._entry_lines += entry.getLines()
 
-    for entry in self._entries:
-      self._entry_lines += entry.getLines()
-
-    self.set_sort_order()
-    self._last_resource_fetch = current_resolution_count
-    self.vals_lock.release()
+      self.set_sort_order()
+      self._last_resource_fetch = current_resolution_count
 
   def _resolve_apps(self, flag_query = True):
     """
