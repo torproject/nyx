dcaddf2ab7f9d2ef8649f98bb6870995ebe0b893
Juan Orti Alcaine <jortialc@redhat.com>
2022-06-27 19:38:34 +0200

    Replace inspect.getargspec usage to support python 3.11

--- nyx/panel/__init__.py
+++ nyx/panel/__init__.py
@@ -78,7 +78,7 @@ class KeyHandler(collections.namedtuple('Help', ['key', 'description', 'current'
       is_match = self._key_func(key) if self._key_func else key.match(self.key)
 
       if is_match:
-        if inspect.getargspec(self._action).args == ['key']:
+        if inspect.getfullargspec(self._action).args == ['key']:
           self._action(key)
         else:
           self._action()
